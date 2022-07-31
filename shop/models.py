b491e05aae070b9e030bd18b2417dadbd6298df5
hexgolem <iamsanyamgoel@gmail.com>
2022-07-31 23:57:11 +0530

    created a tentative product model for shop, registered the model and created a demo product

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Product(models.Model):
+    product_id=models.BigAutoField(primary_key=True)
+    product_name=models.CharField(max_length=300)
+    desc=models.CharField(max_length=300)
+    pub_date=models.DateField("Date",default=datetime.date.today)
+
+    def __str__(self) -> str:
+        return self.product_name
